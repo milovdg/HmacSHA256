789ab0b979a007bc2b11c339b07a70e48f014cef
Dmitry Milov <milovdg@gmail.com>
2023-03-05 01:29:01 +0300

    05/03/2023

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,19 +8,21 @@ public class Main {
 
     public static void main(String[] args) throws NoSuchAlgorithmException, InvalidKeyException {
 
-        String nonce = "ad5f60d4-bbb8-4d42-9d53-010320231529";
-        String shopId = "cyQh2QDReX5ukdou4gofjZ8cDZq5qS";
-        String shopKey = "wdrIGmRrRymF7vanPk4RCQOojGRhh3";
-        String plainString = nonce + "|" + shopId + "|" + shopKey;
+        System.out.println("Signature: "+ getHmacSHA256Signature("ad5f60d4-bbb8-4d42-9d53-010320231529", "cyQh2QDReX5ukdou4gofjZ8cDZq5qS", "wdrIGmRrRymF7vanPk4RCQOojGRhh3"));
+
+    }
+
+    static String getHmacSHA256Signature(String nonce, String shopId, String shopKey) throws NoSuchAlgorithmException, InvalidKeyException {
 
         Mac hmacSha256 = Mac.getInstance("HmacSHA256");
 
         SecretKeySpec secretKeySpec = new SecretKeySpec(shopKey.getBytes(), "HmacSHA256");
         hmacSha256.init(secretKeySpec);
 
+        String plainString = nonce + "|" + shopId + "|" + shopKey;
+
         String signature = Hex.encodeHexString(hmacSha256.doFinal(plainString.getBytes()));
 
-        System.out.println("Signature: "+ signature);
-
+        return Hex.encodeHexString(hmacSha256.doFinal(plainString.getBytes()));
     }
 }
